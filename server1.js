1f3eec99af68957026a3eb0e398b9a52622a5425
faiza.noor <faizanoor3001@gmail.com>
2019-01-06 19:23:08 +0530

    adding files to break monotony

--- server1.js
+++ server1.js
@@ -1,32 +1,23 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-  const {header, method, url} = request;
-  let body = [];
-  request.on('error' , (err) => {
-    console.log("error occured in request", err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-
-    // response stuff
-
-    response.on('error', (err) => {
+  if(request.method === 'POST' && request.url === '/echo') {
+    const {header, method, url} = request;
+    let body = [];
+    request.on('error' , (err) => {
       console.log("error occured in request", err);
-    });
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
 
-    response.statusCode = 200;
-    response.statusMessage = 'Custom success 200';
-    response.setHeader('Content-Type', 'application/json');
-    // above two lines
-    // response.writeHead(200, {'Content-Type', 'application/json'});
-    const responseBody = {header, method, url, body};
-    response.write(JSON.stringify(responseBody));
+      response.on('error', (err) => {
+            console.log("error occured in request", err);
+      });
+      response.end(body);
+      });
+  } else {
+    response.statusCode = 404;
     response.end();
-    // or below line for above two
-    //response.end(JSON.stringify(responseBody));
-
-    // end of response stuff
-  });
+  }
 }).listen(8080);
